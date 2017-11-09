4c287a7c8bba8e364caa1b7797af32c48e637d1c
Parker Jones <pajones3@crimson.ua.edu>
2017-11-09 12:12:46 -0600

    methods of Database Controller

---
+++ src/DatabaseController.java
@@ -0,0 +1,47 @@
+/*
+Parker Jones
+11/09/17
+Database Controller
+ */
+
+public class DatabaseController {
+
+ public void getServiceRecord() {
+	 
+ }
+ 
+ public void newMember() {
+	 
+ }
+
+ public void newProvider() {
+	 
+ }
+ 
+ public void deleteUser() {
+	 
+ }
+ 
+ public void suspendMember() {
+	 
+ }
+ 
+ public void addServiceCode() {
+	 
+ }
+ 
+ public void getUser() {
+	 
+ }
+ 
+ public void searchServiceRecords (int userNum) {
+	 
+ }
+ 
+ public void searchServiceRecords(Date start, Date end) {
+	 
+	 
+	 
+ }
+
+}
