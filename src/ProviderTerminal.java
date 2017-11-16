7b1d5f2f83d58b11624ad36c052b9985b216f0f1
asmarsden <asmarsden@crimson.ua.edu>
2017-11-16 11:49:45 -0600

    updated a bit

--- src/ProviderTerminal.java
+++ src/ProviderTerminal.java
@@ -3,7 +3,7 @@ import java.awt.*;
 //this will be like the starting thing i guess?
 
 /**
- steps:
+ steps
  display login screen, with a box to enter provider number and a button to click if operator
  if operator, switch to operatorterminal
  if provider, validate-- read in provider number text file and turn into array
