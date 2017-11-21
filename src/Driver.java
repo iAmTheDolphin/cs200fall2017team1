36accdaae3798e5c2130abd8e23c27a2c5874897
Parker Jones <pajones3@crimson.ua.edu>
2017-11-21 12:03:57 -0600

    changed database to be able to create a member report object

---
+++ src/Driver.java
@@ -0,0 +1,27 @@
+/*
+ * Parker Jones
+ * 11/21/17
+ * Driver Class
+ */
+
+public class Driver {
+
+    public static void main (String[] args) {
+
+
+        ProviderTerminal providerTerminal = new ProviderTerminal();
+
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("     _________");
+        System.out.println("     _______/");
+        System.out.println("  / /");
+        System.out.println("  | |");
+        System.out.println("  \\  \\ ");
+        System.out.println("   \\     _______");
+        System.out.println("    \\__________/");
+        System.out.println("--------------------------------------------------------------------------");
+
+
+    }
+
+}
