e17a9ec6c14b37d0ce971e71e5cf72ef6fe337ad
Parker Jones <pajones3@crimson.ua.edu>
2017-11-23 21:46:31 -0600

    error fixing and allowing for user input.

--- src/ProviderInterface.java
+++ src/ProviderInterface.java
@@ -5,85 +5,83 @@ public class ProviderInterface {
     public boolean ProviderValidated = false;
     public boolean MemberValidated = false;
 
+
+    DatabaseController database = new DatabaseController();
+
+
     Scanner scan = new Scanner(System.in);
 
-    public void MainMenu(){
+    public void MainMenu() {
 
-        int input;
+        int input = 0;
         System.out.println("\nMenu: \n1.Give Service\n2. Create Service Record\n3. View Provider Directory\n4. Log out\n");
-        if (input == 1){
+
+        input = scan.nextInt();
+
+        if (input == 1) {
             GiveService();
-
-        }
-
-        else if (input == 2){CreateServiceRecord();}
-
-        else if (input == 3){
+        } else if (input == 2) {
+            CreateServiceRecord();
+        } else if (input == 3) {
             ServiceCode temp = new ServiceCode();
             temp.DisplayServiceFile();
-            MainMenu();}
-
-        else if (input == 4){
+            MainMenu();
+        } else if (input == 4) {
             ProviderTerminal temp = new ProviderTerminal();
             temp.start();
-            }
-
-        else{
+        } else {
             System.out.println("Sorry, that is an invalid request. Please try again.");
             MainMenu();
         }
 
 
-
     }
 
-    public void GiveService(){
+    public void GiveService() {
         System.out.println("Please enter your client's member number: ");
-        int MemberNumber= scan.nextInt();
+        int MemberNumber = scan.nextInt();
         Member temp = database.getMember(MemberNumber);
-        if (temp.getUserID()!= -1){
+        if (temp.getUserID() != -1) {
             if (temp.isSuspended) {
                 System.out.println("Sorry, this member is suspended and cannot receive service.");
                 MainMenu();
-            }
-            else{
+            } else {
                 System.out.println("When you are finished, type Y to create the service record, or N to wait until later to create it.");
                 char input2 = TryAgain();
-                if (input2 == 'Y' || input2 == 'y'){
+                if (input2 == 'Y' || input2 == 'y') {
                     ServiceRecord newServiceRecord = new ServiceRecord();
                     newServiceRecord.GenerateServiceRecord();
-                }
-                else {
+                } else {
                     System.out.println("\nYou may create a service record at any time in the main menu.");
                     MainMenu();
                 }
             }
-        }
-        else {
+        } else {
             System.out.println("\nSorry, that member doesn't seem to exist. Try again? (Y/N)");
             char tempx = TryAgain();
-            if (tempx == 'y' || tempx == 'Y'){GiveService();}//try again
-            else {MainMenu();}
+            if (tempx == 'y' || tempx == 'Y') {
+                GiveService();
+            }//try again
+            else {
+                MainMenu();
+            }
         }
     }
 
 
-
-    public void CreateServiceRecord(){//creates a service record object, does GenerateServiceRecord from servicerecord class
+    public void CreateServiceRecord() {//creates a service record object, does GenerateServiceRecord from servicerecord class
         ServiceRecord x = new ServiceRecord();
         x.GenerateServiceRecord();
-         }
-
-
-
+    }
 
 
     public char TryAgain() {
         //System.out.println("\nWould you like to try again? (Y/N)");
-        char input = scan.nextChar();
+        char input = scan.nextLine().charAt(0);
         while (input != 'y' && input != 'Y' && input != 'n' && input != 'N') {
             System.out.println("\nThat is not a valid input. Please try again.");
-            input = scan.nextChar();
+            input = scan.nextLine().charAt(0);
         }
         return input;
-    }}
+    }
+}
