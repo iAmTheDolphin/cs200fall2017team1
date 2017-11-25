5b9a7988f3fb3bbe9449bf667967a51f927d2718
rmmanning <rmmanning@crimson.ua.edu>
2017-11-24 21:31:41 -0600

    added service record data to member report

--- src/MemberReport.java
+++ src/MemberReport.java
@@ -3,15 +3,13 @@
 
 public class MemberReport extends Report {
 	private Member member;
+	ServiceRecord[] recordDB;
 
 	public MemberReport (Member member) {
 		this.member = member;
 		filePath = "MemberReports\\" + member.getUserID();
+		recordDB = DatabaseController.searchServiceRecords(member);
 	}
-	
-	//add array of classes for serviceRecord
-	//ServiceRecord record = new ServiceRecord();
-
 	
 	//writes to file
 	protected void writeToFile() {
@@ -20,7 +18,15 @@ public class MemberReport extends Report {
 				+ "Member Address: " + member.getStreetAddress() + '\n'
 				+ "Member City: " + member.getCity() + '\n'
 				+ "Member State: " + member.getState() + '\n'
-				+ "Member Zip Code: " + member.getZipCode() + '\n';
+				+ "Member Zip Code: " + member.getZipCode() + '\n'
+				+ '\n';
+		
+		for (ServiceRecord record : recordDB) {
+			text += "Date of Service: " + record.ServiceTime + '\n'
+					+ "Provider Name: " + record.ProviderName + '\n'
+					+ "Service Name: " + record.ServiceName + '\n'
+					+ '\n';
+		}
 		reportText.write(text);
 	}
 }
