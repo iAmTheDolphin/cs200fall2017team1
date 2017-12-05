a3089e85a001689c053c59a340c4d2aa6d63e627
hajames <hajames@crimson.ua.edu>
2017-12-05 11:13:40 -0600

    small correction in update provider

--- src/OperatorInterface.java
+++ src/OperatorInterface.java
@@ -334,7 +334,6 @@ public class OperatorInterface {
     }
 
 
-    /*
     private void updateProviderInformation() {
 
         System.out.println("Please enter the ID of the Provider to edit. \"quit\" to exit.");
@@ -432,7 +431,7 @@ public class OperatorInterface {
         DatabaseController.updateProviderEmail(userID, scan.nextLine());
 
     }
-    */
+
 }
 
 
