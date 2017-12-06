24416f151438df3c88d40074ce4b0f72ed36b770
rmmanning <rmmanning@crimson.ua.edu>
2017-12-06 01:05:02 -0600

    added junit test for Member class

---
+++ src/MemberTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MemberTest {
+
+	Member member;
+	
+	@Before
+	public void setUp() throws Exception {
+		member = new Member("first", "last", "address", "city", "state", "zip", "email", "phonenum", 6565650);
+	}
+
+	@Test
+	public void testSuccess() {
+		member.setSuspended(true);
+	}
+	
+	@Test 
+	public void testException() {
+		member.setCity("Tuscaloosa");
+		assertFalse("city" == member.getCity());
+		}
+
+	@Test
+	public void testSanity() {
+		member.setSuspended(true);
+		assertEquals(true, member.isSuspended);
+	}
+}
