40e07725340563cc69fc2fff6a139ba1158adc60
rmmanning <rmmanning@crimson.ua.edu>
2017-12-07 15:58:54 -0600

    added javadoc comments to member test class

--- src/ChocAnTest/MemberTest.java
+++ src/ChocAnTest/MemberTest.java
@@ -9,22 +9,34 @@ import org.junit.Test;
 /**
  * Tests Member class
  * @author Riley Manning
- *
+ * @version 1.0
  */
 
 public class MemberTest {
 
 	Member member;
 	
+	/**
+	 * Constructs/sets up member class
+	 */
+	
 	@Before
 	public void setUp() throws Exception {
 		member = new Member("first", "last", "address", "city", "state", "zip", "email", "phonenum", 6565650);
 	}
 
+	/**
+	 * Tests for success
+	 */
+	
 	@Test
 	public void testSuccess() {
 		member.setSuspended(true);
 	}
+	
+	/**
+	 * Tests for failure
+	 */
 	
 	@Test 
 	public void testException() {
@@ -32,6 +44,10 @@ public class MemberTest {
 		assertFalse("city" == member.getCity());
 		}
 
+	/**
+	 * Tests for sanity
+	 */
+	
 	@Test
 	public void testSanity() {
 		member.setSuspended(true);
