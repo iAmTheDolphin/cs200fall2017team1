7fe88534f84c94ea00afd5902e7515c36ce3c326
hajames <hajames@crimson.ua.edu>
2017-12-08 23:17:52 -0600

    Added javadoc comments to test case

--- src/ChocAnTest/ServiceCodeTest.java
+++ src/ChocAnTest/ServiceCodeTest.java
@@ -19,18 +19,27 @@ class ServiceCodeTest {
 //	int code = 999999;
 //	double fee = 100.00;
 	
-	
+	/**
+	 * creates dummy service code to test
+	 * @throws Exception
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
 		serviceCode = new ServiceCode();  
 	}
 
+	/**
+	 * test for success by assigning a name to a service code and seeing if it returns correctly
+	 */
 	@Test
 	void testForSuccess() {
 		serviceCode.serviceName = "Nutritionist";
 		assertEquals("Nutritionist", serviceCode.serviceName);
 	}
 
+	/**
+	 * test for failure by re-assigning service code value and seeing if the old value remains
+	 */
 	@Test
 	void testForFailure() {
 		serviceCode.serviceCode = 999999;
@@ -38,6 +47,9 @@ class ServiceCodeTest {
 		assertFalse("-1 | -1 | -1.00" == toString);
 	}
 
+	/**
+	 * test for sanity by assigning a service fee, then creating another dummy and ensuring that service fee has been reset
+	 */
 	@Test
 	void testForSanity() {
 		serviceCode.serviceFee = 100.00;
